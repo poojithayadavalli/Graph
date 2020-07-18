ba4c7f0bf9c4c9cf8b5c8afa5f16fa18a85e8984
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-18 16:43:39 +0530

    Update getjunctions.py

--- getjunctions.py
+++ getjunctions.py
@@ -1,1 +1,21 @@
+"""
 
+"""
+
+def addEdge(adj, u, v): 
+    adj[u].append(v)
+
+def junction(adj):
+    l=[]
+    for i in range(len(adj)):
+        if len(adj[i])>1:
+            l.append(str(i))
+    return " ".join(l)
+  
+v=int(input())
+u=int(input())
+adj = [[] for i in range(v)]
+for i in range(u):
+    x,y=map(int,input().split())
+    addEdge(adj,x,y)
+print(junction(adj))
