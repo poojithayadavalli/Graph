90469663d71d6f720855393510cf6a9074cd8458
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-18 18:59:53 +0530

    Update kselectedvertices.py

--- kselectedvertices.py
+++ kselectedvertices.py
@@ -1,7 +1,9 @@
 """
 Given a connected graph with N vertices. The task is to select k vertices from the graph such that all these selected vertices are connected to at least one of the non selected vertex. 
 
-In case of multiple answers print any one of them.
+There are mutltiple combinations of possible answers print any one of them(preferably first possible combination).
+
+Note: The graph is always a complete graph
 
 Input:
 
@@ -25,19 +27,48 @@ Input:
 
 Output
 
-1 3  ({1,2}{1,4},{2,3}{2,4}{3,4} are also possible)
+2 4  ({1,2}{1,4},{2,3}{1,3}{3,4} are also possible)
 
 Example 2:
 
 Input:
 
-3 2
+3 3
 1 2
 1 3
+2 3
 
 output:
 
-1 ({2},{3} are also possible)
+2 ({1},{3} are also possible)
 
 """
+n,m=map(int,input().split())
+vis=[0 for i in range(n)] 
+gr=[[] for i in range(n)] 
+v=[[] for i in range(2)]
+def add_edges(x, y): 
+    gr[x].append(y) 
+    gr[y].append(x) 
+  
+def dfs(x, state):
+    v[state].append(x+1)
+    vis[x] = 1
+    for i in gr[x]: 
+        if (vis[i] == 0): 
+            dfs(i, state ^ 1) 
+def Print_vertices():
+    if (len(v[0]) <= len(v[1])): 
+        for i in v[0]: 
+            print(i,end=" ") 
+    else: 
+        for i in v[1]: 
+            print(i,end=" ") 
+  
+
+for i in range(m):
+    x,y=map(int,input().split())
+    add_edges(x-1, y-1)
+dfs(0, 0)
+Print_vertices() 
 
