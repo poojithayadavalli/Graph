7887a4d073d0e6b61a453d324cdbae86848e1272
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-18 17:36:43 +0530

    Update max-min isolated.py

--- max-min isolated.py
+++ max-min isolated.py
@@ -1,4 +1,39 @@
 """
+Rajesh is working with graphs and he notices a task based on isolated vertices as follows:
+
+Given ‘n’ vertices and ‘m’ edges of a graph. Find the minimum number and maximum number of isolated vertices that are possible in the graph.
+
+Input:
+
+The First line consists of no of vertices n
+Second line consists of no of Edges
+
+Output:
+
+print the minimum and maximum possible isolated vertices 
+
+
+Example 1:
+
+Input:
+
+4
+2
+
+Output:
+
+0 1
+
+Example 2:
+
+Input:
+
+ 5
+ 2
+ 
+ Output:
+ 
+ 1 2
 
 """
 def findsol(n, m) :  
@@ -15,4 +50,5 @@ return maxi, mini
 
 n,m=map(int,input().split())
 x,y=findsol(n,m)
+
 print(x,y,sep=" ")
