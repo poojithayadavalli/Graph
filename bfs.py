6402d3e45b4bb70a53fddcb57a6916dd8ce9e503
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-20 16:58:30 +0530

    Create bfs.py

---
+++ bfs.py
@@ -0,0 +1,38 @@
+"""
+Given a directed graph with V vertices and E edges.Vijay wants to traverse the graph using breadth first traversal so he need your help.
+
+Input:
+
+First line consists of number of vertices V and number of edges E
+Next E lines contains edges connecting two vertices
+
+Output:
+print the vertices of graphs using BFS
+
+Example 1:
+
+Input:
+
+4 3
+1 2
+1 3
+2 3
+
+Output:
+
+1 2 3 4
+
+Example 2:
+
+Input:
+
+5 3
+1 2
+1 3
+2 5
+
+Output:
+
+1 2 3 5 4
+
+"""
