1ee51282872b5bdea343012b873be065622ba324
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-21 14:29:07 +0530

    Create numberofhamiltonian cycles.py

---
+++ numberofhamiltonian cycles.py
@@ -0,0 +1,50 @@
+"""
+
+Koushik is leaning about hamiltonian cycle and he saw a task as follows:
+
+Given an undirected complete graph of N vertices where N > 2. The task is to find the number of different Hamiltonian cycle of the graph.
+
+A graph is said to be complete if each possible vertices is connected through an Edge and Hamiltonian Cycle
+
+is a closed path such that each vertex is visited at most once except the initial vertex. and it is not necessary to visit all the edges
+
+Input:
+
+Input consists of number of vertices of a graph N
+
+Output:
+
+Print the number of hamiltonian cycles possible in the graph
+
+Example:
+
+Input:
+
+6
+
+Output:
+
+120
+
+Example 2:
+
+Input:
+
+5
+
+Output:
+
+12
+
+"""
+
+def Cycles(N):
+    fact = 1
+    result = N - 1
+    i = result 
+    while (i > 0): 
+        fact = fact * i 
+        i -= 1 
+    return fact
+N = int(input())
+Number = Cycles(N)
